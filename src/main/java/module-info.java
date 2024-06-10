b69ff275f92a37aba89000cd4e26cbd5743aab16
luthfiabdllh <ahmadabdllh000@gmail.com>
2024-06-10 10:01:28 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.cashify.cashifyupdate2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.cashify.cashifyupdate2 to javafx.fxml;
+    exports org.cashify.cashifyupdate2;
+}
