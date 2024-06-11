381d7fc38aa8ee81032cb9980b36e7f537589c1e
luthfiabdllh <ahmadabdllh000@gmail.com>
2024-06-11 21:26:13 +0700

    Merge remote-tracking branch 'origin/master'

--- src/main/java/org/cashify/cashifyupdate2/HelloApplication.java
+++ src/main/java/org/cashify/cashifyupdate2/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Hello!");
+        stage.setTitle("login");
         stage.setScene(scene);
         stage.show();
     }
