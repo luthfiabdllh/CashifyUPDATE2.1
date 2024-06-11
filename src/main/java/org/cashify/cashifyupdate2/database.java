068bf997a8dcaa7ecea6b1406585ea57aad2ea8b
luthfiabdllh <ahmadabdllh000@gmail.com>
2024-06-11 19:47:26 +0700

    Merge remote-tracking branch 'origin/master'

--- src/main/java/org/cashify/cashifyupdate2/database.java
+++ src/main/java/org/cashify/cashifyupdate2/database.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class database {
     public static Connection connectDB() {
             try {
-                Class.forName("com.mysql.jdbc.Driver");
+                Class.forName("com.mysql.cj.jdbc.Driver");
                 Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/cashify", "root", ""); // LINK YOUR DATABASE
                 return connect;
             } catch (Exception e) {
